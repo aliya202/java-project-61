5c35d1ba2a71cd51fc9f972c2a844d85b092583e
Aliya <aakhisamova@gmail.com>
2024-02-08 20:59:49 +0400

    added Even game and updated App menue

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String selectedGame = scanner.next();
+        System.out.println();
+
+        switch (selectedGame) {
+            case "1":
+                Cli.greet();
+            case "2":
+                Even.runGame();
+            case "0":
+                System.out.println("Exit");
+                break;
+        }
+
+        scanner.close();
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+import java.util.Random;
+
+public class Even {
+    public static void runGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        int correctAnswersCount = 0; //счетчик успешных раундов
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = (int) (Math.random() * 100);  //генерирую рандомное число
+            System.out.println("Question: " + randomNumber);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.nextLine();
+
+            boolean isEven = randomNumber % 2 == 0; //проверяю четность
+            boolean isCorrect = (isEven && userAnswer.equals("yes")) || (!isEven && userAnswer.equals("no"));
+            //проверяю ответ игрока
+
+            if (isCorrect) {
+                System.out.println("Correct!");
+                correctAnswersCount++;
+            } else {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + (isEven ? "no" : "yes") + "'.");
+                //при ошибке тернарным вычисляю верный ответ
+                System.out.println("Let's try again, " + name + "!");
+                break;
+            }
+        }
+
+        if (correctAnswersCount == 3) {
+            System.out.println("Congratulations, " + name + "!");
+        }
+    }
+}
